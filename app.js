e4fd74a4ec119c8b5bc24653cd9a7eb0740de003
sayantanHack <sayantandatta97@gmail.com>
2018-10-21 02:14:13 +0530

    added new feature

--- app.js
+++
@@ -1,9 +0,0 @@
-
-$button = document.querySelector('img');
-$span = document.querySelector('span');
-$refresh = document.querySelector('button');
-
-function counting(){
-  $span.innerHTML++;
-}
-$button.addEventListener('click', counting)
